bf94e5dc3a59882d68efe8bfddf9b397a4c7e36e
JaymeeDEV <jamiekellybrown@gmail.com>
2019-03-28 23:55:24 +0000

    Final project update

--- src/Sent.vue
+++ src/Sent.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="inbox-body">
-    <app-messages :messages="sentMessages"></app-messages>
-  </div>
+    <div class="inbox-body">
+        <app-messages :messages="sentMessages"></app-messages>
+    </div>
 </template>
 
 <script>
-import Messages from "./Messages.vue";
+    import Messages from './Messages.vue';
 
-export default {
-  props: {
-    data: {
-      type: Object,
-      required: true
+    export default {
+        props: {
+            data: {
+                type: Object,
+                required: true
+            }
+        },
+        computed: {
+            sentMessages() {
+                return this.data.messages.filter(function(message) {
+                    return (message.type == 'outgoing' && !message.isDeleted);
+                });
+            }
+        },
+        components: {
+            appMessages: Messages
+        }
     }
-  },
-  computed: {
-    sentMessages() {
-      return this.data.messages.filter(function(message) {
-        return message.type == "outgoing" && !message.isDeleted;
-      });
-    }
-  },
-  components: {
-    appMessages: Messages
-  }
-};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
 
-export const EVENTBUS = new Vue();
+export const eventBus = new Vue();
 
 new Vue({
-  el: "#app",
-  render: h => h(App)
-});
+    el: '#app',
+    render: h => h(App)
+})
