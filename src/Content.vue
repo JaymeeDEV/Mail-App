0dd78cb0335847c596216371098194565a3b7cb7
JaymeeDEV <jamiekellybrown@gmail.com>
2019-03-28 19:28:00 +0000

    Displaying inbox messages

--- src/Content.vue
+++ src/Content.vue
@@ -4,7 +4,7 @@
       <h3>{{ currentView.title }}</h3>
     </div>
     <keep-alive>
-      <component :is="currentView.tag"></component>
+      <component :is="currentView.tag" :data="currentView.data"></component>
     </keep-alive>
   </aside>
 </template>
@@ -18,12 +18,19 @@ import ViewMessage from "./ViewMessage.vue";
 import { EVENTBUS } from "./main";
 
 export default {
+  props: {
+    messages: {
+      type: Array,
+      required: true
+    }
+  },
   created() {
     EVENTBUS.$on("changeView", data => {
       let temp = [
         {
           tag: data.tag,
-          title: data.title
+          title: data.title,
+          data: data.data || {}
         }
       ];
       this.history = temp.concat(this.history.splice(0));
@@ -34,14 +41,19 @@ export default {
       history: [
         {
           tag: "app-inbox",
-          title: "Inbox"
+          title: "Inbox",
+          data: {
+            messages: null
+          }
         }
       ]
     };
   },
   computed: {
     currentView() {
-      return this.history[0];
+      let current = this.history[0];
+      current.data.messages = this.messages;
+      return current;
     }
   },
   components: {
